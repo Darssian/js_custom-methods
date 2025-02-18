532314b19256b067354e921b4e9e5173aa9d7900
Darssian <korkin.kirill.02@gmail.com>
2025-02-18 18:46:00 +0300

    [fix]: fixed filter

---
+++ myFilter.js
@@ -0,0 +1,22 @@
+function myFilter(array, callback) {
+  if (!Array.isArray(array)) {
+    throw new Error("Первый аргумент должен быть массивом");
+  }
+  if (typeof callback !== "function") {
+    throw new Error("Второй аргумент должен быть функцией");
+  }
+
+  let newArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    if(callback(array[i], i, array)) newArray.push(array[i]);
+  }
+
+  return newArray;
+}
+
+const numbers = [1, 2, 3, 4];
+
+console.log(myFilter(numbers,(item) => {
+    return item % 2 === 0
+}));
